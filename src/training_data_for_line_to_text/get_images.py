2a17e50f5c464ae7796885292fe605d306d96d11
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-06-23 10:19:53 +0530

    downloaded

--- src/training_data_for_line_to_text/get_images.py
+++ src/training_data_for_line_to_text/get_images.py
@@ -2,9 +2,15 @@ import re
 from pathlib import Path
 from openpecha.buda import api
 import requests
+import hashlib
 import rdflib
+import boto3
+import os
 from rdflib import Graph
 
+os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "~/.aws/credentials"
+OCR_OUTPUT_BUCKET = "ocr.bdrc.io"
+S3_client = boto3.client("s3")
 
 def get_work_id(img_group):
     url = f"https://purl.bdrc.io/resource/{img_group}.ttl"
@@ -34,11 +40,11 @@ def get_work_id(img_group):
         work_id = match.group(1)
         return work_id
     else:
-        print("Resource ID not found in the URL.")
+        print("work ID not found in the URL.")
 
 
 def get_img_group_and_image(url):
-    ex = re.compile(r"bdr:(.*)::(.*)/")
+    ex = re.compile(r"bdr:(.*?)::([^/]+)")
     match = ex.search(url)
     img_group = match.group(1)
     img_name = match.group(2)
@@ -50,12 +56,62 @@ def get_url(text_path):
     match = ex.search(text)
     return match.group(1)
 
+def get_no_of_images(text_path):
+    text = Path(text_path).read_text(encoding="utf-8")
+    ex = re.compile(r"-\n-\n(.*)")
+    match = ex.search(text)
+    images_text = match.group(1)
+    images_text = images_text.strip()
+    images = text.split("\n\n")
+    num_images = len(images)
+    return num_images
 
-def main(text_path):
+def get_images(img_name,no_of_images):
+    images = [img_name]
+    pattern = r"^(.*?)(\d+)\.(.*)$"
+    match = re.search(pattern, img_name)
+    if match:
+        prefix_chars = match.group(1)
+        last_number = match.group(2)
+        file_extension = match.group(3)
+    cur_postfix = int(last_number)+1
+    if no_of_images == 1:
+        return images
+    for i in range(no_of_images-1):
+        images.append(prefix_chars+str(cur_postfix)+"."+file_extension)
+        cur_postfix+=1
+    return images
+
+def get_hash(work_id):
+    md5 = hashlib.md5(str.encode(work_id))
+    two = md5.hexdigest()[:2]
+    return two
+
+def download_images(work_id,img_group,images,download_path):
+    if download_path is None:
+        download_path = "./images"
+        Path(download_path).mkdir(parents=False)
+    hash_two = get_hash(work_id)
+    for image in images:
+        object_key = f"Works/{hash_two}/{work_id}/images/{work_id}-{img_group}/{image}"
+        print(object_key)
+        S3_client.download_file(OCR_OUTPUT_BUCKET, object_key, download_path)
+
+        try:
+            S3_client.download_file(OCR_OUTPUT_BUCKET, object_key, download_path)
+            print("Object downloaded successfully.")
+        except Exception as e:
+            print("Error:", str(e))
+
+
+def main(text_path,download_path=None):
     url = get_url(text_path)
     img_group,img_name = get_img_group_and_image(url)
+    no_of_images = get_no_of_images(text_path)
     work_id = get_work_id(img_group)
-    print(work_id)
+    images = get_images(img_name,no_of_images)
+    print(images)
+    download_images(work_id,img_group,images,download_path)
 
 if __name__ == "__main__":
     test_path = "tests/data/test.txt"
